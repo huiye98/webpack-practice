2f56f9c4e7abc96ff893a680c287600ef33f7ae4
yehui <yh263308@alibaba-inc.com>
2020-07-18 17:57:45 +0800

    自动编译的三种方法尝试(感觉dev-server最佳)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
-// 生成html文件
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// 每次自动清理dist文件夹
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -13,18 +11,23 @@ module.exports = {
   },
   output:{
     path:  path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
+    filename: '[name].bundle.js',
+    publicPath: '/',
   },
   devServer:{
-    contentBase:"./dist",
+    contentBase:"./dist", //将 dist 目录下的文件 serve 到 localhost:8080 下。（将资源作为 server 的可访问文件）
     inline: true, //实时刷新
     historyApiFallback: true, //不跳转，单页应用都跳转index.js
     port: '8080'
   },
   plugins:[
+    // 生成html文件
     new HtmlWebpackPlugin({
       title: '管理输出'
     }),
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({
+      // 每次自动清理dist文件夹
+      cleanStaleWebpackAssets: false,
+    }),
   ]
 }
