a154ac904da32e83b5c2387d249d692dacb8f101
yehui <yh263308@alibaba-inc.com>
2020-07-18 16:37:07 +0800

    多入口

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,18 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map' ,
-  entry: __dirname + '/src/index.js',
+  entry:{
+    app: './src/index.js',
+    print: './src/print.js'
+  },
   output:{
     path:  path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
   devServer:{
     contentBase:"./dist",
     inline: true, //实时刷新
     historyApiFallback: true, //不跳转，单页应用都跳转index.js
     port: '8080'
-  },
+  }
 }
