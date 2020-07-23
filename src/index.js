0e52a119f5d9cdd9a4438b6c1be4c1d1fc93baf1
yehui <yh263308@alibaba-inc.com>
2020-07-23 10:59:26 +0800

    用async简化代码

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 // 分离方法3：动态导入, 返回promise
-function getComponent(){
-  // /**/里面是内联注释，会用node.vm运行（沙盒，解析为json对象，用来设置webpack动态加载的配置项）
-  return import(/* webpackChunkName: "lodash" */'lodash').then(({ default: _ })=>{
-    let element = document.createElement('div');
+async function getComponent(){
+    const element = document.createElement('div');
+    const {default:_} = await import(/* webpackChunkName: "lodash" */'lodash');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
-
-  }).catch(err=> 'error occurred while loading!');
 }
 
 getComponent().then(component=>{
