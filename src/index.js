d9328004e23ca8328cab0a4ad28f218e251b8de5
yehui <yh263308@alibaba-inc.com>
2020-07-23 10:25:39 +0800

    分离方法3：动态导入, 返回promise

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import _ from 'lodash';
+// 分离方法3：动态导入, 返回promise
+function getComponent(){
+  // /**/里面是内联注释，会用node.vm运行（沙盒，解析为json对象，用来设置webpack动态加载的配置项）
+  return import(/* webpackChunkName: "lodash" */'lodash').then(({ default: _ })=>{
+    let element = document.createElement('div');
 
-function greeter() {
-  let greet = document.createElement('div');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  greet.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
 
-  return greet;
-};
+  }).catch(err=> 'error occurred while loading!');
+}
 
-document.body.appendChild(greeter());
+getComponent().then(component=>{
+  document.body.appendChild(component);
+})
